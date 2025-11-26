981e94adad2fa360553cc95ab0e3a29994ac667f
Connor Garey <connorgarey1999@gmail.com>
2025-11-26 22:18:12 +0000

    Created an enum class and seperate lambda for printing sizes.

--- src/main.cpp
+++ src/main.cpp
@@ -6,37 +6,72 @@
 #include <format>
 #include <print>
 #include <span>
+#include <string>
 #include <sys/stat.h>
 #include <variant>
-#include <string>
 
-auto processCommands(std::span<const char*> passed_arguments) -> std::expected<std::monostate, std::string>
+namespace
 {
-  for(const auto& arg : passed_arguments.subspan(1))
+enum class SizeType
+{
+  kibibyte,
+  mebibyte,
+  gibibyte,
+};
+auto printSize = [](const auto byte_size, SizeType size_type)
+{
+  switch (size_type)
   {
-    struct stat stat_struct {};
+    case SizeType::kibibyte:
+      {
+        const float file_size{static_cast<float>(byte_size) / 1024.0f};
+        std::println("\tSize: Kibibyte: {:.2}KiB ({}bytes)", file_size, byte_size);
+        break;
+      }
+    case SizeType::mebibyte:
+      {
+        const float file_size{static_cast<float>(byte_size) / (1024.0f * 1024.0f)};
+        std::println("\tSize: Mebibyte: {:.2}MiB ({}bytes)", file_size, byte_size);
+        break;
+      }
+    case SizeType::gibibyte:
+      {
+        const float file_size{static_cast<float>(byte_size) / (1024.0f * 1024.0f * 1024.0f)};
+        std::println("\tSize: Mebibyte: {:.2}MiB ({}bytes)", file_size, byte_size);
+        break;
+      }
+  }
+};
+} // namespace
+
+auto processCommands(std::span<const char *> passed_arguments) -> std::expected<std::monostate, std::string>
+{
+  for (const auto &arg : passed_arguments.subspan(1))
+  {
+    struct stat stat_struct{};
 
     std::println("\033[1mFile:\033[0m {}", arg);
     if (stat(arg, &stat_struct) == -1)
-      return std::unexpected(std::format("Failed to stat file! Error: {}.", strerror(errno))); 
-    
-    if(stat_struct.st_size < 1024) { // Kibibyte
-      const auto file_size {stat_struct.st_size > 1024};
-      std::println("\t\033[1mSize: \033[0m{}\033[1mKiB\033[0m", stat_struct.st_size);
+      return std::unexpected(std::format("Failed to stat file! Error: {}.", strerror(errno)));
+
+    if (stat_struct.st_size > 1024)
+    { // Kibibyte
+      printSize(stat_struct.st_size, SizeType::kibibyte);
       continue;
     }
-    else if (stat_struct.st_size > 1024) { // Mebibyte
-      std::println("\tSize: {}MiB", stat_struct.st_size / 1024);
-      continue; 
+    else if (stat_struct.st_size > 1024 * 1024)
+    { // Mebibyte
+      printSize(stat_struct.st_size, SizeType::mebibyte);
+      continue;
     }
-    std::println("\tSize: {:2}", stat_struct.st_size);
+    std::println("\tSize: {}bytes", stat_struct.st_size);
   }
   return std::monostate{};
 }
 
-auto main(const int argc, const char* argv[]) -> int 
+auto main(const int argc, const char *argv[]) -> int
 {
-  if(const auto result = processCommands(std::span(argv, argc+argv)); !result.has_value())
+  if (const auto result = processCommands(std::span(argv, argc + argv)); !result.has_value())
   {
     std::println("{}", result.error());
     return EXIT_FAILURE;
@@ -44,4 +79,3 @@ auto main(const int argc, const char* argv[]) -> int
 
   return EXIT_SUCCESS;
 }
-
