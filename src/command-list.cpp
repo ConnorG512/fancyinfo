675975df1d9f0c4ae21222338695977e18be548a
Connor Garey <connorgarey1999@gmail.com>
2025-11-27 19:42:21 +0000

    Added help flags.

--- src/command-list.cpp
+++ src/command-list.cpp
@@ -7,6 +7,9 @@ CommandList::CommandList(std::span<char *> command_args) : argument_list_(comman
 {
   application_.add_option("-d,--directory", directory_arg_, "Path to directory");
   application_.add_flag("--human", human_flag_, "Use 1000 instead of 1024 sizing.");
+  
+  application_.set_help_flag("");
+  application_.set_help_all_flag("-h, --help");
 
   application_.parse(argument_list_.size(), argument_list_.data());
   std::println("{}", directory_arg_);
