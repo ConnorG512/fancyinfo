884be7c1a9f6c204daaf9b93b1007d053a80498f
Connor Garey <connorgarey1999@gmail.com>
2025-11-28 23:29:11 +0000

    Properties is now a single template.

--- include/file-size-properties.h
+++ include/file-size-properties.h
@@ -2,47 +2,12 @@
 
 #include <string_view>
 
-namespace SizeType
-{
-struct Properties
-{
-  double DivisionSize(this auto &&self) { self.DivisionSize(); }
-  std::string_view Name(this auto &&self) { self.Name(); }
-};
+template<typename T>
+struct FileSizeProperty {
+    FileSizeProperty(const std::string_view name, double division_size)
+      : name {name}
+      , division_size{division_size} {}
 
-struct Kibibyte : public Properties
-{
-  consteval double DivisionSize() { return 1024.0f; };
-  consteval std::string_view Name() { return "Kibibyte"; };
+    std::string_view name {""};
+    T division_size {0};
 };
-
-struct Mebibyte : public Properties
-{
-  consteval double DivisionSize() { return 1024.0f * 1024.0f; };
-  consteval std::string_view Name() { return "Mebibyte"; };
-};
-
-struct Gibibyte : public Properties
-{
-  consteval double DivisionSize() { return 1024.0f * 1024.0f * 1024.0f; };
-  consteval std::string_view Name() { return "Gibibyte"; };
-};
-
-struct Kilobyte : public Properties
-{
-  consteval double DivisionSize() { return 1000.0f; };
-  consteval std::string_view Name() { return "Kilobyte"; };
-};
-
-struct Megabyte : public Properties
-{
-  consteval double DivisionSize() { return 1000.0f * 1000.0f; };
-  consteval std::string_view Name() { return "Megabyte"; };
-};
-
-struct Gigabyte : public Properties
-{
-  consteval double DivisionSize() { return 1000.0f * 1000.0f * 1000.0f; };
-  consteval std::string_view Name() { return "Gigabyte"; };
-};
-} // namespace SizeType
