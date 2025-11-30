be21240c114b6cbd1d73d4a367be2c00487bbb66
Connor Garey <connorgarey1999@gmail.com>
2025-11-30 00:03:21 +0000

    Changes to the path finding function.

--- include/print-command.h
+++ include/print-command.h
@@ -5,10 +5,11 @@
 #include <expected>
 #include <span>
 #include <string>
+#include <vector>
 
 namespace PrintCommand
 {
 [[nodiscard]] auto CalculateFileSizes(const std::span<char *> file_paths,
-                                      const std::span<const FileSizeProperty<double>> size_types) noexcept
-    -> std::expected<void, std::string>;
+                                      const std::span<const File::Properties, 3> size_types) noexcept
+    -> std::expected<std::vector<std::string>, std::string>;
 };
